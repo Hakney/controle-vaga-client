fd7b5692e47ae71873b3c91fd65b0fc0231e3143
HAKNEY DE SOUSA LIMA <hakneey@gmail.com>
2024-09-04 11:33:35 -0300

    criar form de apartamento

--- src/app/apartamento/apartamento.component.ts
+++ src/app/apartamento/apartamento.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'apartamento',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, FloatLabelModule, InputTextModule, FormsModule, InputNumberModule],
   templateUrl: './apartamento.component.html',
   styleUrl: './apartamento.component.css'
 })
+
 export class ApartamentoComponent {
+  bloco: Number | undefined;
+  apartamento: Number | undefined;
+  morador: String | undefined;
+  telefone: String | undefined;
+  email: String | undefined;
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form data:', form.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }

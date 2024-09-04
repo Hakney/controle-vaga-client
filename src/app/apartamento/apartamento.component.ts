e58b5fcc0f11f351f7d9cab5d8215987cb6e9cf3
HAKNEY DE SOUSA LIMA <hakneey@gmail.com>
2024-09-04 10:29:53 -0300

    first commit

---
+++ src/app/apartamento/apartamento.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'apartamento',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './apartamento.component.html',
+  styleUrl: './apartamento.component.css'
+})
+export class ApartamentoComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApartamentoComponent } from './apartamento/apartamento.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ApartamentoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
